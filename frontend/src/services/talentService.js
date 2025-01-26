45407c60bec31eda5a4be366faa3da10517fb238
conwuka <ogochuku7@gmail.com>
2025-01-26 16:12:34 -0600

    add talentService

---
+++ frontend/src/services/talentService.js
@@ -0,0 +1,20 @@
+import sendRequest from './sendRequest';
+
+const BASE_URL = '/api/talents';
+
+export async function index() {
+  return sendRequest(BASE_URL);
+}
+
+export async function create(skill) {
+  return sendRequest(BASE_URL, 'POST', { skill });
+}
+
+export async function updateTalent(id, skill) {
+  return sendRequest(`${BASE_URL}/${id}`, 'PUT', { skill });
+}
+
+export async function deleteTalent(id) {
+  return sendRequest(`${BASE_URL}/${id}`, 'DELETE');
+}
+
