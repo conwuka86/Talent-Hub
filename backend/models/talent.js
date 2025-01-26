efebbc433162c4e17135282c6443ddb1d55efabe
conwuka <ogochuku7@gmail.com>
2025-01-26 10:53:38 -0600

    add talentSchema

---
+++ backend/models/talent.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User", // Reference to the User model
+      required: true,
+  },
+},
+{
+  timestamps: true, // Adds `createdAt` and `updatedAt` fields
+}
+);
+
+
+
+const talentSchema = new Schema(
+  {
+    skill: { type: String, required: true },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Post', talentSchema);
